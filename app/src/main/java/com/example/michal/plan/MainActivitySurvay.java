85f7159ab5063fa2119428382698becc903346bf
michalk933 <6pak.tv@gmail.com>
2016-11-08 18:03:40 +0100

    Add methot inicialize all components with survay

--- app/src/main/java/com/example/michal/plan/MainActivitySurvay.java
+++ app/src/main/java/com/example/michal/plan/MainActivitySurvay.java
@@ -6,9 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class MainActivitySurvay extends AppCompatActivity {
+
+    private RadioGroup aimRadioGrup,healthRadioGrup,typDietRadioGrup,
+            typDiabetsRadioGrup,activityRadioGrup,sexRadioGrup;
+    private SeekBar weightSeekBar,heightSeekBar,ageSeekBar;
+    private EditText nameEditText, passEditText;
+    private TextView weightTextView,heightTextView,ageTextView;
 
     ViewFlipper f;
     @Override
@@ -18,8 +29,9 @@ public class MainActivitySurvay extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        createComponentsAllLayouts();
 
-        f = (ViewFlipper)findViewById(R.id.solvayViewFlipper);
+        f = (ViewFlipper)findViewById(R.id.solvayViewFlipper);//inicialize ViewFlipper
 
 
 
@@ -27,9 +39,76 @@ public class MainActivitySurvay extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                f.showNext();
+                f.showNext();// change layouts survay
             }
         });
     }
+
+    /*
+        Method createComponentsAllLayouts inicialize all components in content_main_activity_survay
+        And add Listener class
+     */
+    private void createComponentsAllLayouts(){
+        aimRadioGrup = (RadioGroup) findViewById(R.id.aimRadioGrup);
+        healthRadioGrup = (RadioGroup) findViewById(R.id.healthRadioGrup);
+        typDietRadioGrup = (RadioGroup) findViewById(R.id.typDietRadioGrup);
+        typDiabetsRadioGrup = (RadioGroup) findViewById(R.id.typDiabetsRadioGrup);
+        activityRadioGrup = (RadioGroup) findViewById(R.id.activityRadioGrup);
+        sexRadioGrup = (RadioGroup) findViewById(R.id.sexRadioGrup);
+
+        weightSeekBar = (SeekBar)findViewById(R.id.weightSeekBar);
+        heightSeekBar = (SeekBar)findViewById(R.id.heightSeekBar);
+        ageSeekBar = (SeekBar)findViewById(R.id.ageSeekBar);
+
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        passEditText = (EditText)findViewById(R.id.passEditText);
+
+        weightTextView = (TextView)findViewById(R.id.weightTextView);
+        heightTextView = (TextView)findViewById(R.id.heightTextView);
+        ageTextView = (TextView)findViewById(R.id.ageTextView);
+
+
+        aimRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+        healthRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+        typDietRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+        typDiabetsRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+        activityRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+        sexRadioGrup.setOnCheckedChangeListener(onCheckedChangeListener);
+
+        weightSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        heightSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        ageSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
+    }
+
+
+
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+
+    private RadioGroup.OnCheckedChangeListener onCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+        }
+    };
+
+
+
 
 }
