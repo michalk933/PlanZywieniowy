963db44ad629baed56afb8eca9ad14c06a68a3b7
michalk933 <6pak.tv@gmail.com>
2016-11-08 17:43:59 +0100

    Add ViewFlipper in to make little survay. Create layouts for survay, and FAD button change layouts.

--- app/src/main/java/com/example/michal/plan/MainActivitySurvay.java
+++ app/src/main/java/com/example/michal/plan/MainActivitySurvay.java
@@ -6,9 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivitySurvay extends AppCompatActivity {
 
+    ViewFlipper f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +18,16 @@ public class MainActivitySurvay extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        f = (ViewFlipper)findViewById(R.id.solvayViewFlipper);
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                f.showNext();
             }
         });
     }
